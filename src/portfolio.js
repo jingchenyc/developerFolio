aec94947da995a629dc70291d033710f0cf82a62
jing <chenyichun19971110@gmail.com>
2024-10-05 10:20:10 -0700

    update portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -120,7 +120,7 @@ https://fontawesome.com/icons?d=gallery */
 // Education Section
 
 const educationInfo = {
-  display: true, // Set false to hide this section, defaults to true
+  display: false, // Set false to hide this section, defaults to true
   schools: [
     {
       schoolName: "Northeastern University",
@@ -212,8 +212,8 @@ const workExperiences = {
 To know how to get github key look at readme.md */
 
 const openSource = {
-  showGithubProfile: true, // Set true or false to show Contact profile using Github, defaults to true
-  display: true // Set false to hide this section, defaults to true
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: "true" // Set false to hide this section, defaults to true
 };
 
 // Some big projects you have worked on
