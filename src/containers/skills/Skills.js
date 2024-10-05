f1ab36392b589e2aab0e4b18903eb2b0f8d24eaa
jing <chenyichun19971110@gmail.com>
2024-10-05 14:47:45 -0700

    update picture and file

--- src/containers/skills/Skills.js
+++ src/containers/skills/Skills.js
@@ -3,7 +3,7 @@ import "./Skills.scss";
 import SoftwareSkill from "../../components/softwareSkills/SoftwareSkill";
 import {illustration, skillsSection} from "../../portfolio";
 import {Fade} from "react-reveal";
-import codingPerson from "../../assets/lottie/landingPerson.json";
+import codingPerson from "../../assets/lottie/codingPerson.json";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import StyleContext from "../../contexts/StyleContext";
 
